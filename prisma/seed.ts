5510b2372ea91a32f4caba44dbdebcc404e09a5b
PRSMendis <rayhan.mendis@gmail.com>
2022-06-20 17:24:17 +1000

    changing user ID from connect

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -31,8 +31,24 @@ const run = async () => {
       create: {
         email: 'user@test.com',
         password: bcrypt.hashSync('password', salt)
-      }
+      }, 
     })
+    const songs = await prisma.song.findMany({})
+    await Promise.all(
+      new Array(10).fill(1).map(async (_, i) => {
+        return prisma.playlist.create({
+          data: {
+            name: `Playlist #${i + 1}`,
+            userId: user.id,
+            songs: {
+              connect: songs.map((song) => ({
+                id: song.id,
+              })),
+            },
+          },
+        })
+      })
+    )
     }
 
 run()
