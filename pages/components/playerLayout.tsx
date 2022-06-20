273acb9f026c6b2f4daaba19b1c5497fa2864aa6
PRSMendis <rayhan.mendis@gmail.com>
2022-06-20 14:54:13 +1000

    populated list icons

--- pages/components/playerLayout.tsx
+++ pages/components/playerLayout.tsx
@@ -1,12 +1,11 @@
 import { Box } from "../../node_modules/@chakra-ui/layout/dist/chakra-ui-layout.cjs.js";
 
 import Sidebar from "./sidebar"
-
 const playerLayout = ({children}) => {
     return (
         <Box width="100vw" height="100vh">
             <Box position="absolute" top="0" width="250px" left="0">
-                Sidebar
+                <Sidebar></Sidebar>
             </Box>
             <Box marginLeft="250px">
             {children}
--- pages/components/sidebar.tsx
+++ pages/components/sidebar.tsx
@@ -1,8 +1,31 @@
-// import { Box, List, ListItem, ListIcon, Divider,
-//      Center, LinkBox, LinkOverlay } from "../../node_modules/@chakra-ui/layout/dist/chakra-ui-layout.cjs"
-// import { MdHome, MdSearch, MdLibraryMusic, MdFavorite} from  
-// import {MdHome} from "../../node_modules/react-icons";
-import { MdHome } from "react-icons/md"
+import { Box, List, ListItem, ListIcon, Divider,
+     Center, LinkBox, LinkOverlay } from "../../node_modules/@chakra-ui/layout/dist/chakra-ui-layout.cjs"
+import { MdHome, MdSearch, MdLibraryMusic, MdFavorite, MdPlaylistAdd } from "react-icons/md"
+import Image from 'next/image';
+import Link from "next/link.js";
+
+const navMenu = [
+    {
+        name: 'Home',
+        icon: MdHome,
+        route: "/"
+    },
+    {
+        name: 'Search',
+        icon: MdSearch,
+        route: "/search"
+    },
+    {
+        name: 'Your Library',
+        icon: MdLibraryMusic,
+        route: "/library"
+    },
+    {
+        name: 'Home',
+        icon: MdHome,
+        route: "/"
+    }
+]
 
 
 
@@ -11,7 +34,34 @@ import { MdHome } from "react-icons/md"
 
 const Sidebar = () => {
     return (
-        <div>hello world from sidebar</div>
+        <Box 
+            width="100%" height="calc(100vh - 100px)"
+            bg="black" paddingX="5px" color="gray">
+
+            <Box paddingY="20px">
+                <Box width="120px" marginBottom="20px" paddingX="20px">
+                    <Image src="/circle.svg" height={60} width={60}></Image>
+                </Box>
+                <Box marginBottom="20px">
+                    <List spacing={2}>
+                        {/* Implicit return */}
+                        {navMenu.map(menu => (
+                            <ListItem paddingX="20px" fontSize="16px" key={menu.name}>
+                                <LinkBox>
+                                    <Link href={menu.route} passHref>
+                                        <LinkOverlay>
+                                            <ListIcon as={menu.icon} color="white" marginRight="20px"/>
+                                        </LinkOverlay>
+                                    </Link>
+                                </LinkBox>
+                            </ListItem>
+                        ))}
+                    </List>
+
+                </Box>
+
+            </Box>
+        </Box>
     )
 
 }
