e2eb9948bdca4300eda71bb50c8a9b9f91f0badf
PRSMendis <rayhan.mendis@gmail.com>
2022-06-20 14:11:18 +1000

    fixed module issues, relating to import module resolution

--- pages/components/sidebar.tsx
+++ pages/components/sidebar.tsx
@@ -1,6 +1,11 @@
-import { Box, List, ListItem, ListIcon, Divider,
-     Center, LinkBox, LinkOverlay } from "../../node_modules/@chakra-ui/layout/dist/chakra-ui-layout.cjs"
+// import { Box, List, ListItem, ListIcon, Divider,
+//      Center, LinkBox, LinkOverlay } from "../../node_modules/@chakra-ui/layout/dist/chakra-ui-layout.cjs"
 // import { MdHome, MdSearch, MdLibraryMusic, MdFavorite} from  
+// import {MdHome} from "../../node_modules/react-icons";
+import { MdHome } from "react-icons/md"
+
+
+
 
 
 
