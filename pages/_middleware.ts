65021c0a0570e36a8d6e8e1162d2e3b46e689c2d
PRSMendis <rayhan.mendis@gmail.com>
2022-06-21 15:25:14 +1000

    fixed redirect url problem

---
+++ pages/_middleware.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server"
+
+const signedInPages = ['/', '/playlist', '/library']
+
+export default function middleware(req) {
+    if (signedInPages.find((p) => p === req.nextUrl.pathname)) {
+        const token = req.cookies.RAY_ACCES_TOKEN
+
+        if (!token) {
+            const url = req.nextUrl.clone()
+            url.pathname = "/signin"
+            // return NextResponse.redirect('/signin')
+            return NextResponse.redirect(url)
+        }
+    }
+}
--- pages/components/authForm.tsx
+++ pages/components/authForm.tsx
@@ -1,22 +1,36 @@
 import { Box, Flex, Input, Button } from "@chakra-ui/react";
-import { useRouter } from "next/router";
+import Image from "next/image";
+import Router, { useRouter } from "next/router";
 import { FC, useState } from "react";
 import { useSWRConfig } from "swr";
 import { auth } from "../../lib/mutations";
 
-const AuthForm: FC<{mode:string}> = ({mode}) => {
+const AuthForm: FC<{mode: 'signin' | 'signup'}> = ({mode}) => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [isLoading, setLoading] = useState(false)
+    const route = useRouter();
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setLoading(true); 
+
+        await auth(mode, {email, password})
+        setLoading(false)
+        Router.push("/")
+
+    }
 
     return (
         <Box height="100vh" width="100vw" bg="black" color="white">
-            <Flex justify="center" align="center" height="100px">
-                hello
+            <Flex justify="center" align="center" height="100px" borderBottom="white 1px solid">
+                <Image src="/circle.svg" height={60} width={60}></Image>
+                {/* hello */}
             </Flex>
             <Flex justify="center" align="center" height="calc(100vh - 100px)">
                 <Box padding="50px" bg="gray.900" borderRadius="6px">
-                    <form>
+                    <form onSubmit={handleSubmit}>
                         <Input placeholder='email'
                         type='email'
                         onChange={(e) => setEmail(e.target.value)}
@@ -25,7 +39,7 @@ const AuthForm: FC<{mode:string}> = ({mode}) => {
                         type='passsword'
                         onChange={(e) => setPassword(e.target.value)}
                         />
-                        <Button type="submit" bg="green.500" isLoading={isLoading}>
+                        <Button type="submit" bg="green.300" isLoading={isLoading}>
                             {mode}
                         </Button>
 
