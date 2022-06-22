334443b96c0f839a27c349317b6c859591e76572
PRSMendis <rayhan.mendis@gmail.com>
2022-06-23 09:44:39 +1000

    fixing dimensions of gradient layout

--- pages/components/playerLayout.tsx
+++ pages/components/playerLayout.tsx
@@ -7,8 +7,10 @@ const playerLayout = ({children}) => {
             <Box position="absolute" top="0" width="250px" left="0">
                 <Sidebar></Sidebar>
             </Box>
-            <Box marginLeft="250px">
-            {children}
+            <Box marginLeft="250px" marginBottom="100px">
+                <Box height="calc(100vh - 100px)">
+                    {children}
+                </Box>
             </Box>
             <Box position="absolute" left="0" bottom="0">
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import GradientLayout from './components/gradientLayout'
 
 export default function Home() {
-  return (<div>home</div>)
+  return (<GradientLayout color='red'>
+
+  </GradientLayout>)
 }
