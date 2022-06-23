78307dc125de06003743f3add7314f152e34f10d
PRSMendis <rayhan.mendis@gmail.com>
2022-06-23 10:21:36 +1000

    working gradient page

--- pages/components/gradientLayout.tsx
+++ pages/components/gradientLayout.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex } from "@chakra-ui/layout";
+import { Box, Flex, Text } from "@chakra-ui/layout";
 import { color, Image } from "@chakra-ui/react";
 
 const gradientLayout = ({
@@ -8,13 +8,24 @@ const gradientLayout = ({
     subtitle,
     title,
     description,
-    roundImage
+    roundImage,
 
     }) => {
         return (
             <Box height="100%" overflowY="auto" bgGradient={`linear(${color}.500 0%, ${color}.600 15%, ${color}.700 40%, rgba(0,0,0,0.95) 75% )`}>
                 <Flex bg={`${color}.600`} padding="40px" align="end">
-                    hello
+                    <Box padding="20px">
+                        <Image
+                            src = {image}
+                            boxSize="160px" boxShadow="2xl"
+                            borderRadius={roundImage ? '100%' : '3px'} />
+                    </Box>
+                    <Box padding="20px" lineHeight="40px" color="white">
+                        <Text fontSize="x-small" fontWeight="bold" casing="uppercase" >{subtitle}</Text>
+                        <Text fontSize='6xl'>{title}</Text>
+                        <Text fontSize="x-small" > {description}</Text>
+
+                    </Box>
                 </Flex>
 
             </Box>
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,12 @@ import styles from '../styles/Home.module.css'
 import GradientLayout from './components/gradientLayout'
 
 export default function Home() {
-  return (<GradientLayout color='red'>
+  return (<GradientLayout color='red' roundImage='true' 
+  subtitle='profile' 
+  title='Rayhan Mendis' 
+  description='10 public playlists'
+  image="https://dl.dropboxusercontent.com/s/bgiv0ssz3xpotz9/peep.png?dl=0"
+  >
 
   </GradientLayout>)
 }
